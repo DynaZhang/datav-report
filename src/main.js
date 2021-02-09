cd15f907596a09ee35246445a14a116cbc05c3c5
zhangzhulei <13339687922@163.com>
2021-02-10 02:53:47 +0800

    临时提交

--- src/main.js
+++ src/main.js
@@ -1,21 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import FilterList from '@/filters'
 
 import router from './router'
 import Echarts from 'echarts'
 import './plugins/element'
-import './plugins/vcharts'
 import VueEcharts from 'vue-echarts'
 
 import './assets/styles/common.scss'
 
-FilterList.forEach(item => {
-  Vue.filter(item.name, item.handler)
-})
-
 Vue.prototype.$echarts = Echarts
-Vue.prototype.$bmap = window.BMapGL
 
 Vue.component('v-chart', VueEcharts)
 
